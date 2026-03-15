b0e372d23b51a2e12812eb08d86aab87dd90f43f
cjm0423 <cjm042352@naver.com>
2026-03-15 18:06:06 +0900

    chore: Spring Boot 초기 세팅 완료

---
+++ src/test/java/com/example/P26/P26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.P26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
